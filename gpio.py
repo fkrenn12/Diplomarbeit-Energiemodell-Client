b0ec016ca9592b7baadca31803da3d44e2fb6d49
fkrenn12 <fkrenn@nanet.at>
2025-06-24 13:49:29 +0200

    init

---
+++ gpio.py
@@ -0,0 +1,56 @@
+from machine import Pin, PWM
+from adc import get_voltage
+import neopixel
+
+NEO_PIXEL_PIN = 8
+ADC_PINS = [2, 3, 4, 5]
+PWM_PINS = [14, 15, 18, 19, 20, 21, 22]
+DIGITAL_PINS = [14, 15, 18, 19, 20, 21, 22]
+
+rgb = neopixel.NeoPixel(Pin(NEO_PIXEL_PIN), 1)
+rgb[0] = (0, 0, 0)
+rgb.write()
+
+
+def set_pin(pin, value):
+    pin = Pin(pin, Pin.OUT)
+    pin.value(value)
+
+
+def get_pin(pin):
+    pin = Pin(pin, Pin.IN)
+    return pin.value()
+
+
+def read_adc(pin):
+    return f"{get_voltage(pin):.2f}"
+
+
+def set_pwm(pin, freq=500, duty_percent=0):
+    duty = duty_percent * 65535 // 100
+    #pwm = PWM(Pin(pin))
+    #pwm.freq(freq)
+    #pwm.duty_u16(duty)
+    PWM(Pin(pin), freq=freq, duty_u16=duty)
+
+
+def execute(pin=None, typ=None, direction=None, value=None, freq=None, duty_percent=None):
+    try:
+        pin = int(pin) if pin else None
+        value = int(value) if value else None
+        freq = int(freq) if freq else None
+        duty_percent = int(duty_percent) if duty_percent else None
+
+        if typ == "pwm" and pin in PWM_PINS:
+            set_pwm(pin, freq, duty_percent)
+        elif typ == "adc" and pin in ADC_PINS:
+            return read_adc(pin)
+        elif typ == "digital" and pin in DIGITAL_PINS:
+            if direction is None:
+                print(f"set_pin({pin}, {value})")
+                set_pin(pin, value)
+            if direction == "?":
+                return str(int(get_pin(pin)))
+        return str()
+    except Exception as e:
+        return f"{e}"
--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 import network
 import time
+import machine
 from umqtt.simple import MQTTClient
 import time
+from ubinascii import hexlify
+from machine import unique_id
+from adc import get_address
+from gpio import execute
+from gpio import rgb
 
+# General
+MAC = hexlify(unique_id(), ":").decode("utf-8")
+UID = (int(MAC.replace(":", "")[-6:], 16) % 100000)
+CLIENT_ADDRESS = get_address()
 # WiFi-Konfiguration
-SSID = "LAWIG14-FlexBox"  # Ersetze mit deinem WLAN-Namen
-PASSWORD = "wiesengrund14"  # Ersetze mit deinem WLAN-Passwort
+SSID = "LAWIG14-FlexBox"
+PASSWORD = "wiesengrund14"
 
 # MQTT-Konfiguration
 MQTT_BROKER = "192.168.0.132"  # IP-Adresse oder Domain des MQTT-Brokers
 MQTT_PORT = 8883
-MQTT_TOPIC = "object/1/coil/1"
-MQTT_CLIENT_ID = "XIAO_ESP32_1234"
-MQTT_USER = "admin"  # Optional: Benutzername für MQTT-Auth
-MQTT_PASSWORD = "admin"  # Optional: Passwort für MQTT-Auth
+MQTT_TOPIC_ROOT_IN = "to-client"
+MQTT_TOPIC_ROOT_OUT = "from-client"
+MQTT_CLIENT_ID = f"ESP32-C6-Client_{UID}"
+MQTT_USER = "admin"  # Benutzername für MQTT-Auth
+MQTT_PASSWORD = "admin"  # Passwort für MQTT-Auth
 
-def mqtt_callback(topic, msg):
-    print(f"Empfangene Nachricht: {topic.decode('utf-8')} -> {msg.decode('utf-8')}")
-    
+
 def connect_mqtt():
     try:
         print(f"Verbinde mit MQTT-Broker {MQTT_BROKER}...")
-        client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, ssl=True, port=MQTT_PORT, user=MQTT_USER, password=MQTT_PASSWORD)
+        client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, ssl=True, port=MQTT_PORT, user=MQTT_USER,
+                            password=MQTT_PASSWORD)
         client.connect()
         print("Mit MQTT-Broker verbunden!")
         return client
     except Exception as e:
         print(f"MQTT-Verbindung fehlgeschlagen: {e}")
         return None
+
 
 def connect_wifi():
     wlan = network.WLAN(network.STA_IF)  # WLAN-Interface im Station-Modus
@@ -41,38 +52,106 @@ def connect_wifi():
         while not wlan.isconnected():  # Warten, bis Verbindung hergestellt wird
             retries += 1
             print(retries)
-            if retries >= 20:  # Abbruch nach 20 Versuchen (~10 Sekunden)
+            if retries >= 20:  # Abbruch nach 20 Versuchen (~20 Sekunden)
                 print("Verbindung fehlgeschlagen.")
                 return False
-            time.sleep(1)
+            rgb[0] = (0, 20, 0)
+            rgb.write()
+            time.sleep(0.1)
+            rgb[0] = (0, 0, 0)
+            rgb.write()
+            time.sleep(0.9)
+
     print(f"Mit WiFi verbunden! IP-Adresse: {wlan.ifconfig()[0]}")
-    return True 
+    return True
+
+
+def handle_mqtt(topic, msg) -> str | None:
+    try:
+        topic = topic.replace(f"{MQTT_TOPIC_ROOT_IN}/{CLIENT_ADDRESS}/", "")
+        topic_count = len(topic.split("/"))
+        duty_percent = None
+        freq = None
+        direction = None
+        if topic_count == 4 and "pwm" in topic:
+            pin, typ, freq, duty_percent = topic.split("/")[-4:]
+        elif topic_count == 3 and "digital" in topic:
+            pin, typ, direction = topic.split("/")[-3:]
+        elif topic_count == 2 and ("adc" in topic or "digital" in topic):
+            pin, typ = topic.split("/")[-2:]
+        else:
+            raise Exception('Invalid Topic')
+        print(f"Pin: {pin}, Typ: {typ}, Direction: {direction}, Freq: {freq}, Duty: {duty_percent}")
+        return execute(pin=pin, typ=typ, value=msg, direction=direction, freq=freq, duty_percent=duty_percent)
+
+    except Exception as e:
+        # return None
+        return str(e)
 
 
 def main():
+    def mqtt_callback(topic, msg):
+        rgb[0] = (0, 0, 20)
+        rgb.write()
+        try:
+            print(f"Empfangene Nachricht: {topic.decode()} -> {msg.decode()}")
+            topic = topic.decode()
+            response = handle_mqtt(topic, msg.decode())
+            if response is not None:
+                topic = topic.replace(MQTT_TOPIC_ROOT_IN, MQTT_TOPIC_ROOT_OUT)
+                topic = topic.replace("/?", "")
+                print(f"Nachricht an den Broker: {topic} {response}")
+                mqtt_client.publish(topic, response)
+                print(f"Nachricht gesendet: {topic} -> {response}")
+        finally:
+            rgb[0] = (0, 0, 0)
+            rgb.write()
+
     # Versuchen, eine Verbindung aufzubauen
     if not connect_wifi():
         print("Keine Verbindung möglich. Neustart...")
         machine.reset()  # Neustart bei Verbindungsproblemen
 
     mqtt_client = connect_mqtt()
-    # if not mqtt_client:
-    #    return
-    
-    mqtt_client.set_callback(mqtt_callback)
-    mqtt_client.subscribe(b"esp32/subscription")
+    if not mqtt_client:
+        print("Keine Verbindung mit MQTT-Broker möglich. Neustart...")
+        machine.reset()
 
-    # Beispiel: Senden von Nachrichten an den Broker
+    rgb[0] = (20, 0, 0)
+    rgb.write()
+    mqtt_client.set_callback(mqtt_callback)
+    subscribe_topic = f"{MQTT_TOPIC_ROOT_IN}/{CLIENT_ADDRESS}/+/digital/#"
+    print(f"Abonniere auf Topic: {subscribe_topic}")
+    mqtt_client.subscribe(subscribe_topic.encode())
+    subscribe_topic = f"{MQTT_TOPIC_ROOT_IN}/{CLIENT_ADDRESS}/+/adc/#"
+    print(f"Abonniere auf Topic: {subscribe_topic}")
+    mqtt_client.subscribe(subscribe_topic.encode())
+    subscribe_topic = f"{MQTT_TOPIC_ROOT_IN}/{CLIENT_ADDRESS}/+/pwm/#"
+    print(f"Abonniere auf Topic: {subscribe_topic}")
+    mqtt_client.subscribe(subscribe_topic.encode())
+    rgb[0] = (0, 0, 0)
+    rgb.write()
+    counter = 0
     while True:
         try:
-            message = "Hallo von XIAO ESP32"
-            mqtt_client.publish(MQTT_TOPIC, message)
-            print(f"Nachricht gesendet: {MQTT_TOPIC} -> {message}")
-            time.sleep(5)  # Nach 5 Sekunden neue Nachricht senden
+            counter += 1
+            if counter % 600 == 0:  # every minute
+                mqtt_client.ping()
+                continue
+            mqtt_client.check_msg()
+            if counter % 10 == 0:  # every second
+                time.sleep(0.09)
+                rgb[0] = (5, 0, 0)
+                rgb.write()
+                time.sleep(0.01)
+                rgb[0] = (0, 0, 0)
+                rgb.write()
+            else:
+                time.sleep(0.1)
+
         except Exception as e:
             print(f"Fehler beim Senden der MQTT-Nachricht: {e}")
-            mqtt_client.disconnect()
-            break
+            machine.reset()
 
 
 if __name__ == "__main__":
